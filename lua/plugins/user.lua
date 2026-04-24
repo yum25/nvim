32d82ad7632ef2685342b71481e05c7b418c8327
yumarie <yumarie@umich.edu>
2026-04-24 04:19:58 -0400

    prioritize prettier for js/ts

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -193,4 +193,23 @@ return {
       },
     },
   },
+  -- Conform formatters
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = false,
+      },
+    },
+  },
 }
