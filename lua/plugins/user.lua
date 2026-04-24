20a805d4c5dea23320437254fb0cbaf5bc8163d3
yumarie <yumarie@umich.edu>
2026-04-24 04:21:53 -0400

    clean unnecessary config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -206,10 +206,6 @@ return {
         css = { "prettier" },
         html = { "prettier" },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = false,
-      },
     },
   },
 }
