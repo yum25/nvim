d7c2829d84611c6125bc23ca4ec49e7aa324eb4f
yumarie <yumarie@umich.edu>
2026-06-23 16:52:58 -0400

    toggle viewable files for snacks explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = { hidden = true, ignored = true },
+        files = { hidden = true, ignored = true },
+        smart = { hidden = true },
+      },
+    },
+    explorer = {
+      hidden = true,
+    },
+  },
+}
